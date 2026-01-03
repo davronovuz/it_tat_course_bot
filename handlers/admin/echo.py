7fc3e400e2eb1e934cbab141309ed92d23b82fcc
davronovuz <davronovtatu@gmail.com>
2026-01-03 16:39:26 +0500

    saas saas

--- handlers/admin/echo.py
+++ handlers/admin/echo.py
@@ -2,9 +2,11 @@ from aiogram import types
 from loader import dp
 
 
-
 @dp.message_handler(content_types=['document'])
 async def get_file_id(message: types.Message):
     file_id = message.document.file_id
     file_name = message.document.file_name
-    await message.reply(f"Fayl nomi: {file_name}\n\nID: `{file_id}`", parse_mode="Markdown")
+    await message.reply(
+        f"Fayl nomi: {file_name}\n\nID: <code>{file_id}</code>",
+        parse_mode="HTML"
+    )
